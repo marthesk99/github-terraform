bea71923d45f1f46e26500eaeaf1427f4d9baf76
Marthe Solland Koch <marthe.s.koch@tisipfagskole.no>
2024-11-12 17:10:24 +0100

    Test

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Testing trigger for GitHub Actions
 locals {
   workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
   rg_name          = "${var.rg_name}-${local.workspace_suffix}"
