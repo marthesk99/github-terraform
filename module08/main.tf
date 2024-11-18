b1c43e26fc7848f39aa751df4c691e5f8c6fcbb3
Marthe Solland Koch <marthe.s.koch@tisipfagskole.no>
2024-11-18 14:09:37 +0100

    changing

--- module08/main.tf
+++ module08/main.tf
@@ -1,8 +1,8 @@
 locals {
-  workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
+  workspace_suffix = terraform.workspace == "default" ? "" : terraform.workspace
   rg_name          = "${var.rg_name}-${local.workspace_suffix}"
   sa_name          = "${var.sa_name}${random_string.random_string.result}"
-  dynamic_content = "<h1>Web page created with Terraform: ${terraform.workspace}</h1>"
+  dynamic_content  = "<h1>Web page created with Terraform: ${terraform.workspace}</h1>"
 }
 
 resource "random_string" "random_string" {
@@ -11,17 +11,15 @@ resource "random_string" "random_string" {
   upper   = false
 }
 
-# Create Resource Group
-resource "azurerm_resource_group" "rg_web" {
+resource "azurerm_resource_group" "rg" {
   name     = local.rg_name
   location = var.location
 }
 
-# Create Storage Account
-resource "azurerm_storage_account" "sa_web" {
-  name                     = local.sa_name 
-  resource_group_name      = azurerm_resource_group.rg_web.name
-  location                 = azurerm_resource_group.rg_web.location
+resource "azurerm_storage_account" "sa" {
+  name                     = local.sa_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -30,10 +28,9 @@ resource "azurerm_storage_account" "sa_web" {
   }
 }
 
-# Add a index.html file to the storage account
-resource "azurerm_storage_blob" "index_html" {
+resource "azurerm_storage_blob" "blob" {
   name                   = var.index_document
-  storage_account_name   = azurerm_storage_account.sa_web.name
+  storage_account_name   = azurerm_storage_account.sa.name
   storage_container_name = "$web"
   type                   = "Block"
   content_type           = "text/html"
@@ -41,5 +38,5 @@ resource "azurerm_storage_blob" "index_html" {
 }
 
 output "primary_web_endpoint" {
-  value = azurerm_storage_account.sa_web.primary_web_endpoint
+  value = azurerm_storage_account.sa.primary_web_endpoint
 }
